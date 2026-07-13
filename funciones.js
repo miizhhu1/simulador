2d3e420e12f5e6746e666b2b59b1ef11d36dd445
Miizhhu1 <miizhhu@gmail.com>
2026-07-12 19:20:02 -0500

    crear calcular y calcularDisponible y mostrar en pantalla el resultado

--- funciones.js
+++ funciones.js
@@ -1,1 +1,9 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos,egresos){
+    let disponible = ingresos-egresos;
+    if (disponible>0){
+        return disponible.toFixed(2);
+    }else{
+        return 0;
+    }
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,19 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    //recuperamos propiedades de la caja de texto
+    let componenteIngresos = document.getElementById("txtIngresos");
+    let componenteEgresos = document.getElementById("txtEgresos");
+
+    //recuperamos el valor de las cajas de texto en string
+    let ingresosStr = componenteIngresos.value;
+    let egresosStr = componenteEgresos.value;
+
+    //convertimos el texto a numeros
+    let ingresos = parseFloat(ingresosStr);
+    let egresos=parseFloat(egresosStr);
+
+    let disponible = calcularDisponible(ingresos,egresos);
+    
+    let spnDisponible=document.getElementById("spnDisponible");
+    spnDisponible.textContent=disponible;
+}
